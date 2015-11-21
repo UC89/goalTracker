d3e7ea317e105539e1c297482dfa804929eb70da
Taylor Somma <taylor.somma@gmail.com>
2015-11-20 22:19:49 -0500

    Signup form close and submit buttons are functional. Basic layout established for goal grid. Working on progress bar

--- client/signupform/signupform.js
+++ client/signupform/signupform.js
@@ -2,5 +2,8 @@ Template.signupTemplate.events = {
 	'click #cancel-signup' : function() {
 		console.log('Cancel signup');
 		$('.signup-form').css('display','none');
+	},
+	'click #signup-submit' : function() {
+		console.log('Submit Form');
 	}
 }

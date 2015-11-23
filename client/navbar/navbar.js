12c08474ac0d3dff69e71169b4e64da54686fd2a
Taylor Somma <taylor.somma@gmail.com>
2015-11-23 16:24:13 -0500

    Basic signup, login, and logout functionality implemented

--- client/navbar/navbar.js
+++ client/navbar/navbar.js
@@ -6,5 +6,18 @@ Template.navbar.events = {
 	'click #signup-button' : function() {
 		console.log('Clicked Signup Button');
 		$('.signup-form').css('display','block');
+	},
+	'click #login-button' : function(event,template) {
+		console.log('Clicked login button');
+		var username = template.find('#login-username').value;
+		var password = template.find('#login-password').value;
+		console.log('username: '+username);
+		console.log('password: '+password);
+		Meteor.loginWithPassword(username,password);
+	},
+	'click #logout-button' : function()
+	{
+		console.log('Logging out: '+Meteor.user()['username']);
+		Meteor.logout();
 	}
 }
--- client/signupform/signupform.js
+++ client/signupform/signupform.js
@@ -3,7 +3,15 @@ Template.signupTemplate.events = {
 		console.log('Cancel signup');
 		$('.signup-form').css('display','none');
 	},
-	'click #signup-submit' : function() {
+	'click #signup-submit' : function(event,template) {
 		console.log('Submit Form');
+		var createdAt = new Date();
+		var userName = template.find('#usernameInput').value;
+		var userEmail = template.find('#userEmail').value;
+		var userPassword1 = template.find('#userPassword1').value;
+		var userPassword2 = template.find('#userPassword2').value;
+		console.log('Username: '+userName);
+		var newUser = createUser(userName,userPassword1,userEmail,createdAt);
+		Accounts.createUser(newUser);
 	}
 }

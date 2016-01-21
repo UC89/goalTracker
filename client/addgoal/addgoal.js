8d8fdda8610a3acbbfc0e598dedfc0bb4736d50d
Taylor Somma <taylor.somma@gmail.com>
2016-01-21 00:14:21 -0500

    Add weights to different states of objectives, calculate percentage of each state, fill progress bar on dashboard accordingly

--- client/addgoal/addgoal.js
+++ client/addgoal/addgoal.js
@@ -75,17 +75,37 @@ Template.addgoal.events = {
 		var objectivesAdded = 0;
 		var totalWeight = 0;
 		var goalsAdded = 0;
-
+		var pendingWeight = 0;
+		var completeWeight = 0;
+		var inProgressWeight = 0;
 		goalNumbers.forEach(function(item,index,array){
-			console.log('ADding goal objectives')
+			console.log('Adding goal objectives');
 			var newObjective = {};
 			var objectiveName = template.find('#goal-objective-'+item).value;
 			var objectiveWeight = parseInt(template.find('#goal-objective-wgt-'+item).value);
-			console.log('Objective name: '+objectiveName)
-			newObjective[objectiveName]=objectiveWeight;
+			newObjective['isComplete'] = objectiveCompleteStatus(item);
+			console.log('Objective name: '+objectiveName);
+			newObjective['objectiveWeight']=objectiveWeight;
+			newObjective['objectiveName'] = objectiveName;
 			totalWeight+=objectiveWeight;
 			newGoalList.push(newObjective);
 			goalsAdded+=1;
+		});
+
+		newGoalList.forEach(function(item,index,array) {
+			console.log('item: '+ item);
+			console.log('index: '+ index);
+			console.log('array: '+ array);
+			if (item.isComplete == 0) {
+				pendingWeight += item.objectiveWeight
+			}
+			else if(item.isComplete == 1) {
+				inProgressWeight += item.objectiveWeight
+			}
+			else if(item.isComplete == 2) {
+				completeWeight += item.objectiveWeight
+			}
+
 		});
 
 		var files = document.getElementById('goal-image');
@@ -98,6 +118,9 @@ Template.addgoal.events = {
     const goalPictureUrl = goalImage.url({brokenIsFine: true});
     newGoalObject['goalPictureUrl'] = goalPictureUrl;
     newGoalObject['objectives'] = newGoalList
+    newGoalObject['pendingPercentage'] = Math.round((pendingWeight/totalWeight)*100)
+    newGoalObject['inProgressPercentage'] = Math.round((inProgressWeight/totalWeight)*100)
+    newGoalObject['completedPercentage'] = Math.round((completeWeight/totalWeight)*100)
 
     Goals.insert(newGoalObject);
 
@@ -118,8 +141,26 @@ var createNewGoalTask = function(indexNum) {
 
 	var newObjective = '<div class="goal-line" id="'+containerId+'"><label class="col-sm-2 text-center">Objective</label><div class="col-sm-6"><input type="text" class="form-control" id="'+objectiveId+
 		'" placeholder="Objective"></div>'+
-		'<div class="col-sm-2"><input type="text" class="form-control" id="'+objectiveWeightId+'"placeholder="wgt"'+'</div></div>'+'<div class="col-sm-1"><button type="button" id="'+goalToRemove+'" class="btn btn-danger center-button button-padding remove-goal"><span class="glyphicon glyphicon-remove"></span></button></div>'+
+		'<div class="col-sm-2"><input type="text" class="form-control" id="'+objectiveWeightId+'"placeholder="wgt"'+'</div></div>'+'<div class="col-sm-1"><button type="button" id="'+goalToRemove+'" class="btn btn-danger center-button button-padding remove-goal">delete</button></div>'+
 		'<div class="col-sm-1"><button type="button" class="btn btn-danger center-button button-padding completion-status" id="'+goalObjectiveComplete+'""><span class="glyphicon glyphicon-option-horizontal"></span></button></div>'
 		return newObjective
+}
+
+function objectiveCompleteStatus(objectiveId) {
+	var objectiveId = $('#goal-objective-complete-'+objectiveId).attr('class')
+	if (objectiveId == 'btn btn-info center-button button-padding completion-status') {
+		console.log(1)
+		return 1
+	}
+	else if (objectiveId == 'btn btn-danger center-button button-padding completion-status') {
+		return 0
+	}
+	else if (objectiveId == 'btn btn-success center-button button-padding completion-status') {
+		return 2
+	}
+	else {
+		return NaN
+	}
+
 }
 
